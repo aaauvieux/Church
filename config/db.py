605c2029a4ad7a1dc871093c9da42844d086652d
aaauvieux <aaauvieux@gmail.com>
2023-07-18 16:48:42 -0300

    Cambio de servidor de base de datos

--- config/db.py
+++ config/db.py
@@ -11,11 +11,13 @@ user_db = os.getenv('USER_DB')
 password_db = os.getenv('PASSWORD_DB')
 
 print(server)
+print(type(server_port))
 print(db)
 print(user_db)
 print(password_db)
 
-engine = create_engine("mysql+pymysql://sql7631486:ra5aZaGJBR@sql7.freemysqlhosting.net:3306/sql7631486")
+engine = create_engine("mysql+pymysql://4vdsco0al8bl02g10m90:pscale_pw_4SRWPf7EZpKMOwenhVMipEnMFJTcvoXAaOAB5BOBDLh@aws.connect.psdb.cloud:3306/church")
+#engine = create_engine("mysql+pymysql://sql7631486:ra5aZaGJBR@sql7.freemysqlhosting.net:3306/sql7631486")
 #engine = create_engine(f"mysql+pymysql://{user_db}:{password_db}@{server}:{server_port}/{db}")
 #engine = create_engine("mysql+pymysql://mysql_adrian:123456@localhost:3306/church")
 
