054f97aeff900b90f6ab67aa1cd7aa0685712613
aaauvieux <aaauvieux@gmail.com>
2023-07-23 18:10:42 -0300

    Nueva configuracion de Base de Datos

--- config/db.py
+++ config/db.py
@@ -11,14 +11,27 @@ user_db = os.getenv('USER_DB')
 password_db = os.getenv('PASSWORD_DB')
 
 print(server)
-print(type(server_port))
+print(server_port)
 print(db)
 print(user_db)
 print(password_db)
 
-engine = create_engine("mysql+pymysql://4vdsco0al8bl02g10m90:pscale_pw_4SRWPf7EZpKMOwenhVMipEnMFJTcvoXAaOAB5BOBDLh@aws.connect.psdb.cloud:3306/church")
-#engine = create_engine("mysql+pymysql://sql7631486:ra5aZaGJBR@sql7.freemysqlhosting.net:3306/sql7631486")
+engine = create_engine("mysql+pymysql://sql7631486:ra5aZaGJBR@sql7.freemysqlhosting.net:3306/sql7631486")
+meta_data = MetaData()
+
+"""
+#ssl_args = {'ssl_ca': ca_path}
+#engine = create_engine("mysql+mysqlconnector://<user>:<pass>@<addr>/<schema>",connect_args=ssl_args)
+
+#conn_string = "mysql+pymysql://4vdsco0al8bl02g10m90:pscale_pw_4SRWPf7EZpKMOwenhVMipEnMFJTcvoXAaOAB5BOBDLh@aws.connect.psdb.cloud:3306/church"
+
+#conn_args = {
+#    "ssl-mode": "DISABLED",
+#}
+#engine = create_engine(conn_string, connect_args=conn_args)
+
+#    "sslrootcert": "/etc/ssl/certs/ca-certificates.crt",
+#engine = create_engine("mysql+pymysql://4vdsco0al8bl02g10m90:pscale_pw_4SRWPf7EZpKMOwenhVMipEnMFJTcvoXAaOAB5BOBDLh@aws.connect.psdb.cloud:3306/church",connect_args={'sslmode':'allow'})
 #engine = create_engine(f"mysql+pymysql://{user_db}:{password_db}@{server}:{server_port}/{db}")
 #engine = create_engine("mysql+pymysql://mysql_adrian:123456@localhost:3306/church")
-
-meta_data = MetaData()
+"""
